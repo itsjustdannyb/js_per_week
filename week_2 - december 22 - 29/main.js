461172085e056279309607f4a95f057f127576eb
itsjustdannyb <itsjustdannyb@danielbamilosin@gmail.com>
2024-12-23 14:18:16 +0100

    styling

--- week_2 - december 22 - 29/main.js
+++ week_2 - december 22 - 29/main.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const joke_text = document.querySelector("#joke");
+const counter = document.querySelector(".counter");
+let count = 0;
 
 async function getJoke() {
   const response = await fetch("https://icanhazdadjoke.com/", {
@@ -13,7 +15,13 @@ async function getJoke() {
   return joke;
 }
 
-getJoke()
-  // .then((joke) => console.log(joke))
-  .then((joke) => (joke_text.textContent = joke["joke"]))
-  .catch((error) => console.error("failed to get joke", error));
+document.addEventListener("click", function () {
+  console.log("click");
+  getJoke()
+    // .then((joke) => console.log(joke))
+    .then((joke) => (joke_text.textContent = joke["joke"]))
+    .catch((error) => console.error("failed to get joke", error));
+  count++;
+  counter.textContent = `${count} / ∞`;
+  console.log(count);
+});
