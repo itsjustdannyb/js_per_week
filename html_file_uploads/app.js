7086a9736d949a100925ebaf8c56c1f525431ae5
itsjustdannyb <itsjustdannyb@danielbamilosin@gmail.com>
2025-03-01 17:44:21 +0100

    upload images

---
+++ html_file_uploads/app.js
@@ -0,0 +1,24 @@
+// "use strict";
+const dropArea = document.getElementById("drop-area");
+const inputFile = document.getElementById("input-file");
+const imageView = document.getElementById("img-view");
+
+inputFile.addEventListener("change", uploadImage);
+
+function uploadImage() {
+  // uploaded file, you have to convert to image link
+  let imgLink = URL.createObjectURL(inputFile.files[0]);
+  imageView.style.backgroundImage = `url(${imgLink})`;
+  imageView.textContent = "";
+  imageView.style.border = 0;
+}
+
+dropArea.addEventListener("dragover", function (e) {
+  e.preventDefault();
+});
+
+dropArea.addEventListener("drop", function (e) {
+  e.preventDefault();
+  inputFile.files = e.dataTransfer.files;
+  uploadImage();
+});
